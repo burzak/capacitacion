e0d3bde6647f2fd66f8af9f4fc607052049b8497
Mauro Sebastian Torrez <mstorrez@dlatv.net>
2019-04-26 16:11:02 -0300

    comientando

--- ModeloExamen/testCasiBuscaMinas.php
+++ ModeloExamen/testCasiBuscaMinas.php
@@ -7,55 +7,106 @@ use PHPUnit\Framework\TestCase;
 
 final class testCasiBuscaMinas extends TestCase
 {
+  protected function setUp() : void
+  {
+    $this->bm = new BuscaMinas(5,5);
+    
+  }
 
   public function testExisteCasiBuscaMinas() 
   {
     $this->assertTrue(class_exists("BuscaMinas"));
   }
 
+  public function testAgregarFueraDelTablero() 
+  {
+    $this->assertFalse($this->bm->agregarMina(10,10));
+  }
+
+  public function testAgregarMinaEnMina()
+  {
+    $this->bm->agregarMina(0,0);
+    $this->assertFalse($this->bm->agregarMina(0,0));
+
+  }
+
+
   
   public function testAgregarMinasCorrectamente() 
   {
-    $this->bm = new BuscaMinas(5,5);
-    $this->bm->agregarMina(0,0);
-    $this->bm->agregarMina(2,1);
-    $this->bm->agregarMina(4,3);
-    $this->bm->agregarMina(0,4);
-    $this->bm->agregarMina(2,4);
-
-    $this->assertEquals(array (-9999,2,1,1,0)    ,$this->bm->minas[0],'FALLO AL MARCAR VALORES EN TABLA');
-    $this->assertEquals(array (1,2,-9999,1,0)    ,$this->bm->minas[1],'FALLO AL MARCAR VALORES EN TABLA');
-    $this->assertEquals(array (0,1,  1,  2,1)    ,$this->bm->minas[2],'FALLO AL MARCAR VALORES EN TABLA');
-    $this->assertEquals(array (1,2,1,2,-9999)    ,$this->bm->minas[3],'FALLO AL MARCAR VALORES EN TABLA');
-    $this->assertEquals(array (-9999,2,-9999,2,1),$this->bm->minas[4],'FALLO AL MARCAR VALORES EN TABLA');
     
-    //AGREGAR COMENTARIO :   
+    $this->assertTrue($this->bm->agregarMina(0,0));
+    $this->assertTrue($this->bm->agregarMina(2,1));
+    $this->assertTrue($this->bm->agregarMina(4,3));
+    $this->assertTrue($this->bm->agregarMina(0,4));
+    $this->assertTrue($this->bm->agregarMina(2,4));
+
+
+    $this->assertEquals(2,$this->bm->jugar(1,0));
+    $this->assertEquals(1,$this->bm->jugar(2,0));
+    $this->assertEquals(1,$this->bm->jugar(3,0));
+    $this->assertEquals(0,$this->bm->jugar(4,0));
+
+    $this->assertEquals(1,$this->bm->jugar(0,1));
+    $this->assertEquals(2,$this->bm->jugar(1,1));
+    $this->assertEquals(1,$this->bm->jugar(3,1));
+    $this->assertEquals(0,$this->bm->jugar(4,1));
+   
+    $this->assertEquals(0,$this->bm->jugar(0,2));
+    $this->assertEquals(1,$this->bm->jugar(1,2));
+    $this->assertEquals(1,$this->bm->jugar(2,2));
+    $this->assertEquals(2,$this->bm->jugar(3,2));
+    $this->assertEquals(1,$this->bm->jugar(4,2));
+
+    $this->assertEquals(1,$this->bm->jugar(0,3));
+    $this->assertEquals(2,$this->bm->jugar(1,3));
+    $this->assertEquals(1,$this->bm->jugar(2,3));
+    $this->assertEquals(2,$this->bm->jugar(3,3));
+
+    $this->assertEquals(2,$this->bm->jugar(1,4));
+    $this->assertEquals(2,$this->bm->jugar(3,4));
+    $this->assertEquals(1,$this->bm->jugar(4,4));
+    
+    
   }
 
   public function testSacarMinasCorrectamente() 
   {
-    $this->bm = new BuscaMinas(5,5);
+    
     $this->bm->agregarMina(0,0);
     $this->bm->agregarMina(2,1);
     $this->bm->agregarMina(4,3);
     $this->bm->agregarMina(0,4);
     $this->bm->agregarMina(2,4);
 
-    $this->bm->sacarMina(2,1);
-    $this->bm->sacarMina(4,3);
-    $this->bm->sacarMina(0,4);
-    $this->bm->sacarMina(2,4);
+    $this->assertTrue($this->bm->sacarMina(2,1));
+    $this->assertTrue($this->bm->sacarMina(4,3));
+    $this->assertTrue($this->bm->sacarMina(0,4));
+    $this->assertTrue($this->bm->sacarMina(2,4));
 
-    $this->assertEquals(array (-9999,1,0,0,0),$this->bm->minas[0],'FALLO AL MARCAR VALORES EN TABLA');
-    $this->assertEquals(array (1,1,0,0,0)    ,$this->bm->minas[1],'FALLO AL MARCAR VALORES EN TABLA');
-    $this->assertEquals(array (0,0,0,0,0)    ,$this->bm->minas[2],'FALLO AL MARCAR VALORES EN TABLA');
-    $this->assertEquals(array (0,0,0,0,0)    ,$this->bm->minas[3],'FALLO AL MARCAR VALORES EN TABLA');
-    $this->assertEquals(array (0,0,0,0,0)    ,$this->bm->minas[4],'FALLO AL MARCAR VALORES EN TABLA');
+    $this->assertEquals(0,$this->bm->jugar(2,1));
+    $this->assertEquals(0,$this->bm->jugar(4,3));
+    $this->assertEquals(0,$this->bm->jugar(0,4));
+    $this->assertEquals(0,$this->bm->jugar(2,4));
     
   }
+
+  public function testSacarMinaNoEsta() 
+  {
+    $this->assertFalse($this->bm->sacarMina(4,4));
+  }
+
+  public function testSacarDosVeces()
+  {
+    $this->bm->agregarMina(0,0);
+    $this->bm->sacarMina(0,0);
+    $this->assertFalse($this->bm->sacarMina(0,0));
+
+  }
+
   public function testPerdio()
   {
-    $this->bm = new BuscaMinas(5,5);
+    
     $this->bm->agregarMina(0,0);
     $this->bm->agregarMina(2,1);
     $this->bm->agregarMina(4,3);
@@ -69,7 +120,7 @@ final class testCasiBuscaMinas extends TestCase
 
   public function testGano()
   {
-    $this->bm = new BuscaMinas(5,5);
+    
     $this->bm->agregarMina(0,0);
     $this->bm->agregarMina(2,1);
     $this->bm->agregarMina(4,3);
