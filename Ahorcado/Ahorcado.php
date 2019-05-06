7aa8cdedc26b3abc993b998b0b92a188cf5d282a
nacho gomez <nacho.gomezcc@outlook.com>
2019-05-06 09:49:43 -0300

    asdasd

--- Ahorcado/Ahorcado.php
+++ Ahorcado/Ahorcado.php
@@ -1,11 +1,10 @@
 <?php
-
-require './vendor/autoload.php';
-
+ 
 class Ahorcado {
   private $palabra;
+  private $letras = array();
   private $intentos;
-
+ 
   public function __construct($palabra, $intentos) {
     $this->palabra = $palabra;
     $this->intentos = $intentos;
@@ -13,12 +12,47 @@ class Ahorcado {
   public function damePalabra() {
     return $this->palabra;
   }
-
-  public function dameIntentos() {
+ 
+  public function pasarLetra($letra) {
+    if (!empty($this->letras[$letra])) {
+      $this->intentos--;
+      return false;
+    }
+    $this->letras[$letra] = 1;
+    if (strpos($this->palabra, $letra) === false) {
+      $this->intentos--;
+      return false;
+    }
+    return true;
+  }
+ 
+  public function dameIntentosRestantes() {
     return $this->intentos;
   }
-
-  public function mostrarTablero() {
-    return "_ _ _ _ _";
+ 
+  public function gano() {
+    $count = 0;
+    for($i=0; $i<strlen($this->palabra); $i++) {
+      if (!empty($this->letras[ $this->palabra[$i] ])) {
+        $count++;
+      }
+    }
+    return $count == strlen($this->palabra);
+  }
+ 
+  public function perdio() {
+    return $this->intentos == 0;
+  }
+ 
+  public function mostrar() {
+    $mostrar = "";
+    for($i=0; $i<strlen($this->palabra); $i++) {
+      if (empty($this->letras[ $this->palabra[$i] ])) {
+        $mostrar .= ' _ ';
+      } else {
+        $mostrar .= ' ' . $this->palabra[$i] . ' ';
+      }
+    }
+    return $mostrar;
   }
 }
